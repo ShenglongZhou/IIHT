93dcaa28ca68739464423977737376b9c47b094b
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2019-10-08 17:52:33 +0100

    Add files via upload

---
+++ IIHT/examples/logistic_regression/logistic_regression.m
@@ -0,0 +1,34 @@
+function [out1,out2] = logistic_regression(x,data)
+
+    Tx    = find(x);
+    m     = length(data.b);   
+    if ~isempty(Tx)
+    Ax    = data.A(:,Tx)*x(Tx); 
+    eAx   = exp(Ax);
+    else
+    Ax    = zeros(m,1);
+    eAx   = ones(m,1);
+    end
+        
+    mu    = 1e-6/m;
+    
+    if sum(eAx)==Inf 
+       Tpos = find(Ax>300); 
+       Tneg = setdiff(1:m,Tpos);
+       obj  = sum(log(1+eAx(Tneg)))+sum(Ax(Tpos))-sum(data.b.*Ax);                
+    else
+       obj  = sum(log(1+eAx)-data.b.*Ax); 
+    end
+    
+    out1 = obj/m;                                     %objective function
+        
+    if nargout>1 
+       eXx  = 1./(1+eAx);
+       out2 = data.At*((1-data.b-eXx)/m) + mu*x;      %gradien
+    end
+        
+     
+end
+
+
+
