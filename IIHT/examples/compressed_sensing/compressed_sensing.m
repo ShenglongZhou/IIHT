d8bcfa01857f783b57befa75a31d18c976edcd27
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2019-10-08 17:49:44 +0100

    Create compressed_sensing.m

---
+++ IIHT/examples/compressed_sensing/compressed_sensing.m
@@ -0,0 +1,17 @@
+function [out1,out2] = compressed_sensing(x,data)    Tx  = find(x);
+
+    if ~isempty(Tx)   
+    Axb  = data.A(:,Tx)*x(Tx)-data.b;
+    else
+    Axb  =  -data.b;
+    end
+
+    out1 = sum(Axb.*Axb)/2;                %objective function 
+    
+    if  nargout>1 
+    out2 = data.At*Axb;                    %gradien          
+    end
+
+end
+
+
