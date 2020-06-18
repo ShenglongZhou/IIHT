263ba8c327e51fb428cc0662bfb907dbad2a2ace
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-06-18 15:43:41 +0100

    Update demon_CS.m

--- IIHT/demon_CS.m
+++ IIHT/demon_CS.m
@@ -1,33 +1,44 @@
 % demon compressed sensing problems with randomly generated data
 clc; clear; close all;
 
-n       = 2000;  
+n       = 10000;  
 m       = ceil(n/4); 
-s       = ceil(0.01*n);                      
- 
-% % You could input any data including (data.A, data.At, data.b) 
-I         = randperm(n); 
-Tx        = I(1:s);
-x         = zeros(n,1);  
-x(Tx)     = randn(s,1);
-data.xopt = x;
-data.A    = randn(m,n);
-data.At   = data.A';
-data.b    = data.A(:,Tx)*data.xopt(Tx);  
+s       = ceil(0.01*n); 
+test    = 1; 
 
-% Or you could input data from our data generation function
-% ExMat = 1;
-% MatType = {'GaussianMat','PartialDCTMat'}; 
-% data    = compressed_sensing_data(MatType{ExMat}, m,n,s,0);
+switch test
+  case 1  % Input any data including (data.A, data.At, data.b) 
+       I         = randperm(n); 
+       Tx        = I(1:s);
+       x         = zeros(n,1);  
+       x(Tx)     = randn(s,1);
+       data.xopt = x;
+       data.A    = randn(m,n);
+       data.At   = data.A';
+       data.b    = data.A(:,Tx)*data.xopt(Tx);  
+  case 2 % Input data from our data generation function
+       ExMat   = 1;
+       MatType = {'GaussianMat','PartialDCTMat'}; 
+       data    = compressed_sensing_data(MatType{ExMat}, m,n,s,0);
+  case 3 % Input a real data including (data.A, data.At, data.b) 
+       load 'DrivFace.mat'; load 'nlab.mat';   %'identity.mat';
+       [m,n]     = size(A);
+       s         = ceil(0.01*n);
+       scale     = svds(A,1);
+       data.A    = A/scale; 
+       data.b    = y/scale; 
+       data.At   = data.A'; 
+end
 
 
 fun      = str2func('compressed_sensing');
 func     = @(x)fun(x,data);  
 pars.tol = 1e-6*sqrt(n);
-out      = IIHT(n,s,func,pars); 
+out      = IIHTnew(n,s,func,pars); 
 
-fprintf('\n Sample size:       m=%d, n=%d\n', m,n);
-fprintf(' Recovery time:    %6.3fsec\n',  out.time);
+fprintf('\n Sample size:       %dx%d\n', m,n);
+fprintf(' Recovery time:     %.3fsec\n',  out.time);
+fprintf(' Objective value:   %5.2e\n', out.obj);
 if isfield(data,'xopt')
 fprintf(' Recovery accuracy: %5.2e\n\n', ...
 norm(out.x-data.xopt)/norm(data.xopt));
