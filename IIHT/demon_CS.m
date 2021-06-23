3ce8b50155470e59446e9cab0c570656fdc49cbf
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-06-23 18:31:59 +0100

    Update demon_CS.m

--- IIHT/demon_CS.m
+++ IIHT/demon_CS.m
@@ -28,6 +28,5 @@ out      = IIHT('CS',n,s,data,pars);
 fprintf(' CPU time:          %.3fsec\n',  out.time);
 fprintf(' Objective:         %5.2e\n',  out.obj);
 fprintf(' Sample size:       %dx%d\n', m,n);
-if isfield(data,'xopt') && s<=100
-   ReoveryShow(data.xopt,out.x,[1000, 550, 400 200],1)
-end
+RecoverShow(data.xopt,out.x,[1000 500 500 250],1)
+
